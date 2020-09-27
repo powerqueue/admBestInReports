d0678e5e1dbc61728e83e0c96cc36db678983809
Nestor David <santiago.nestor.d@gmail.com>
2020-09-27 14:20:33 -0400

    initial single-spa commit and push

---
+++ bestin-reports-admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bestin-reports-admin-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bestin-reports-admin';
+}
---
+++ bestin-reports-admin/src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bestin-reports-admin-empty-route',
+  template: '',
+})
+export class EmptyRouteComponent {
+}
